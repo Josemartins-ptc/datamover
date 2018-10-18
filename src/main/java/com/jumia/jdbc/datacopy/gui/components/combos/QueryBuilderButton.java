cfc2e71ad9e6f168c774ef8a247d773a1f52cad9
José Martins <jose.martins@jumia.com>
2018-10-18 09:05:43 +0100

    Merge branch 'master' of https://github.com/Josemartins-ptc/datamover
    # Conflicts:
    #	src/main/java/com/jumia/jdbc/datacopy/gui/Gui.java

--- src/main/java/com/jumia/jdbc/datacopy/gui/components/combos/QueryBuilderButton.java
+++ src/main/java/com/jumia/jdbc/datacopy/gui/components/combos/QueryBuilderButton.java
@@ -1,7 +1,42 @@
 package com.jumia.jdbc.datacopy.gui.components.combos;
 
+
+import java.util.UUID;
+
+import com.googlecode.lanterna.gui2.Button;
+import com.jumia.jdbc.datacopy.gui.guicontrollers.GuiController;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author josemartins 2018-10-17
  */
+@Slf4j
+@Data
 public class QueryBuilderButton {
+
+	private final UUID uuid;
+	private final GuiController guiController=new GuiController();
+	private final Button queryBuilderButton;
+
+	public QueryBuilderButton() {
+		queryBuilderButton=new Button("Build Query");
+		queryBuilderButton.addListener(listener);
+		uuid=UUID.randomUUID();
+	}
+
+	private Button.Listener listener = new Button.Listener() {
+		@Override
+		public void onTriggered(Button button) {
+			log.info("Build query event received...building query");
+			guiController.queryBuild();
+		}
+	};
+
+	{
+
+	}
+
+
+
 }
